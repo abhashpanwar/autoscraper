a93ef1539f72a985dcdd90dc400ee9a37525004c
Abhash Panwar <55841948+abhashpanwar@users.noreply.github.com>
2021-04-27 16:53:48 +0530

    files updated

--- app.py
+++ app.py
@@ -2,28 +2,51 @@ from flask import Flask, render_template, request
   
 app = Flask(__name__)
 
-
 @app.route("/",methods=['GET'])  
 def home():    
 
+    #when user search it
     if request.args.get('search'):
+        #inputs
         search = request.args.get('search')
-        search_data = searchquery(search)
+        sortby = request.args.get('sortby','relevanceblender')
+        
+        #call function to retrieve data
+        search_data,url = searchquery(search,sortby)
         data_length = len(search_data)
-        return render_template("index.html",data = {'query':search,'searchData':search_data,'totalRecords':data_length}) 
+        
+        #show to user
+        return render_template("index.html",data = {'query':search,'sortby':sortby,'searchData':search_data,'totalRecords':data_length}) 
+    
+    #default data_length when no search
     data_length = -1
     return render_template("index.html",data = {'query':"",'searchData':"d",'totalRecords':data_length}) 
-def searchquery(search):
+def searchquery(search,sortby):
+    #load library
     from autoscraper import AutoScraper
+    import pandas as pd
+    import time
 
-    amazon_url="https://www.amazon.in/s?k={}&s=price-desc-rank".format(search)
+    #define url
+    amazon_url="https://www.amazon.in/s?k={}&s={}".format(search,sortby)
 
+    #creating object and loading
     amazon_scraper = AutoScraper()
-    amazon_scraper.load('amazon_in')
+    amazon_scraper.load('amazon_in')    
     
+    #get data
     data = amazon_scraper.get_result_similar(amazon_url, group_by_alias=True)
+
+    #combine data into tuple to show it to user
     search_data = tuple(zip(data['Title'],data['ImageUrl'],data['Price'],data['Rating']))
+
+    #creating dataframe so that user can download it in csv format
+    df = pd.DataFrame(columns=['Query','Title','Price','Reviews','ImageUrl'])
+    for i in range(len(search_data)):
+        df.loc[len(df)] = [search,search_data[i][0],search_data[i][2],search_data[i][3],search_data[i][1]]
+    df.to_csv("static/searchedData.csv",index=False)
     
-    return search_data
+    #returing data
+    return search_data,amazon_url
 if __name__ == "__main__":
     app.run(debug=True)
